f97cae6f86060b6690d0e6782831a4a3ef0dd27d
Megan Kuo <megan.kuo@live.com>
2021-03-06 02:00:11 -0800

    admin ejs

---
+++ session.js
@@ -0,0 +1,29 @@
+{
+HGmYQHo0Z_C12a4gruT2V7XYkDOlzZ0Y: '{
+  "cookie": {
+    "originalMaxAge": 86400000,
+    "expires": "2021-03-07T06:58:51.040Z",
+    "secure": false,
+    "httpOnly": true,
+    "path": "/"
+  },
+  "passport": { "user": 3 }
+} ',
+'w5uWRF-clbMPg4UOK9PpSbAZPQOzDbPB': '{
+"cookie": { "originalMaxAge": 86400000, "expires": "2021-03-07T06:58:58.752Z", "secure": false, "httpOnly": true, "path": "/" }, "passport": { "user": 61397197 }}'
+}
+
+{
+  'OSyKQvCzRq_qYYr-xE6BhyAbhzXRXj4h': '{"cookie":{"originalMaxAge":86400000,"expires":"2021-03-07T08:42:02.327Z","secure":false,"httpOnly":true,"path":"/"},"passport":{"user":4}}'
+}
+{
+  "cookie": {
+    "originalMaxAge": 86400000,
+      "expires": "2021-03-07T08:42:02.327Z", "secure": false,
+      "httpOnly": true,
+        "path": "/"
+  },
+  "passport": {
+    "user": 4
+  }
+}

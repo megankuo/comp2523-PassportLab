60fb3e39b9f330510285cbf660ca61f0739cc44e
Megan Kuo <megan.kuo@live.com>
2021-03-06 02:28:07 -0800

    session info displays on admin but cant see more than 1 session

--- routes/indexRoute.js
+++ routes/indexRoute.js
@@ -20,7 +20,7 @@ router.get('/dashboard', ensureAuthenticated, (req, res) => {
 //---------- Secret Admin Route -----------//
 // localhost:8081/admin
 router.get('/admin', isAdmin, (req, res) => {
-  // const allSessions = req.sessionStore.sessions;
+  const allSessions = req.sessionStore.sessions;
   // console.log(allSessions);
   const details = [];
 //   req.sessionStore.all((error, sessions) => {
@@ -33,28 +33,28 @@ router.get('/admin', isAdmin, (req, res) => {
 //       console.log(details);
 //     }
 //   });
-  const allSessions = req.sessionStore.all((err, sessions) => {return sessions});
-  console.log(allSessions);
-  details.push({
-    userID: 10000,
-    sessionID: 'qwerty'
-  })
-  // for (const sessionID in allSessions) {
-  //   // let sessionInfo = JSON.parse(sessionID);
-  //   let sessionInfo = JSON.parse(allSessions[sessionID]);
-  //   // let sessionInfoObj = JSON.parse(sessionInfoStr);
-  //   console.log(sessionInfo);
-  //   console.log(sessionInfo.passport.user);
-  //   // console.log(sessionInfoObj);
-  //   details.push({
-  //     userID: sessionInfo.passport.user,
-  //     sessionID: sessionID,
-  //   });
+  // const allSessions = req.sessionStore.all((err, sessions) => {return sessions});
+  // console.log(allSessions);
+  // details.push({
+  //   userID: 10000,
+  //   sessionID: 'qwerty'
+  // })
+  for (const sessionID in allSessions) {
+    // let sessionInfo = JSON.parse(sessionID);
+    let sessionInfo = JSON.parse(allSessions[sessionID]);
+    // let sessionInfoObj = JSON.parse(sessionInfoStr);
+    console.log(sessionInfo);
+    console.log(sessionInfo.passport.user);
+    // console.log(sessionInfoObj);
+    details.push({
+      userID: sessionInfo.passport.user,
+      sessionID: sessionID,
+    });
   console.log("pooooops");
   console.log(details);
   // }
 
   res.render('admin', { user: req.user, details });
-});
+}});
 
 module.exports = router;
